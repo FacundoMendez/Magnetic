99a918d948357aa84606bc20deffbecf8785a965
Facundo Mendez <facundo.mendez@live.com.ar>
2023-09-19 01:56:37 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import gltf from "vite-plugin-gltf"; // (b) Vite
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    gltf({
-/*       transforms: [ textureResize({ size: [ 1024, 1024 ] }) ] */
-    }) 
+  plugins: [
+    gltf() 
   ]
 })
