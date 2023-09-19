a92a8235a1cfc86093e92a765779b44cafe6b3b7
Facundo Mendez <facundo.mendez@live.com.ar>
2023-09-19 01:50:47 -0300

    fix

--- model/main.js
+++ model/main.js
@@ -45,7 +45,7 @@ gltfLoader.setDRACOLoader(dracoLoader);
 let mobileNode
 let mobileNode2
 
-gltfLoader.load("./magnetic.glb", (gltf) => {
+gltfLoader.load("/magnetic.glb", (gltf) => {
   const magnetic = gltf.scene;
   magnetic.rotation.y = Math.PI;
   magnetic.scale.set(0.9, 0.9, 0.9);
